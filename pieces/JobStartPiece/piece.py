1a4391db3aa3a9e4a1c2a4951e039cbbcf3eaac1
Vanclei Picolli <vancleip@gmail.com>
2024-07-04 10:30:53 -0300

    Simple test

--- pieces/JobStartPiece/piece.py
+++ pieces/JobStartPiece/piece.py
@@ -2,16 +2,18 @@ from domino.base_piece import BasePiece
 from .models import InputModel, OutputModel
 import json
 import requests
+import os
 
 class JobStartPiece(BasePiece):
 
     def piece_function(self, input_data: InputModel):
 
         self.logger.info(f"Job Started")
-        x = requests.get('http://domino-rest:8000/health-check')
+        # x = requests.get('http://domino-rest:8000/health-check')
 
-        self.logger.info(x.text)        
-        
+        # self.logger.info(x.text)        
+        for name, value in os.environ.items():
+            self.logger.info("{0}: {1}".format(name, value))        
         
 
         message = input_data.model_dump(mode='json')
